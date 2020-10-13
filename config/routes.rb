46330bc50c02db06656940a6e4d4d369525ef3bb
yota-matsushita <yotabattu@hotmail.com>
2020-10-13 18:11:42 +0900

    messagesテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   get 'messages/index'
-  root to: "messages#index"
+  #root to: "messages#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
